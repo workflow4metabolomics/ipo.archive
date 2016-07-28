14c22331da8b2807ea05e01bbb80c3f7920b7340
lecorguille <lecorguille@users.noreply.github.com>
2016-07-28 10:08:36 +0200

    add parameters

--- galaxy/ipo/ipo.r
+++ galaxy/ipo/ipo.r
@@ -11,7 +11,7 @@ sink(log_file, type = "output")
 # ----- PACKAGE -----
 cat("\tPACKAGE INFO\n")
 #pkgs=c("xcms","batch")
-pkgs=c("parallel","BiocGenerics", "Biobase", "Rcpp", "mzR", "xcms","rsm","igraph","CAMERA","IPO","batch")
+pkgs=c("parallel","BiocGenerics", "Biobase", "Rcpp", "mzR", "xcms","snow","rsm","igraph","CAMERA","IPO","batch")
 for(pkg in pkgs) {
   suppressWarnings( suppressPackageStartupMessages( stopifnot( library(pkg, quietly=TRUE, logical.return=TRUE, character.only=TRUE))))
   cat(pkg,"\t",as.character(packageVersion(pkg)),"\n",sep="")
