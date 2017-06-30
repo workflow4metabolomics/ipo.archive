14f344e5df7db00558f90ddb04530edef08dd37e
RJMW <r.j.weber@bham.ac.uk>
2017-06-30 16:03:55 +0100

    Fix workflow4metabolomics/ipo#18

--- galaxy/ipo/ipo4xcmsSet.r
+++ galaxy/ipo/ipo4xcmsSet.r
@@ -107,7 +107,6 @@ if(exists("zipfile") && (zipfile!="")) {
     # Because IPO only want raw data in its working directory
     dir.create("ipoworkingdir")
     setwd("ipoworkingdir")
-    file.symlink(zipfile, ".")
 
     #unzip
     suppressWarnings(unzip(zipfile, unzip="unzip"))
@@ -120,8 +119,6 @@ if(exists("zipfile") && (zipfile!="")) {
     if (length(directories) == 1) directory = directories
 
     cat("files_root_directory\t",directory,"\n")
-
-    file.remove(zipfile)
 
 
 }
